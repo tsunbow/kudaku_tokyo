8fd9fff9f4244f152874f2111af07d75116186e4
tsunbow <kazuki_tsuyama@bita.jp>
2026-02-16 16:59:22 +0900

    Add initial content and OGP meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<meta
+				name="description"
+				content="砕区は、既存のやり方やルールに縛られないクリエイティブ・エリア。プロデューサー／映像作家／アートディレクター／写真家／空間デザイナーなどが交わりクライアントとの自由な発想とコラボレーションで心と人を動かすアウトプットを産み、愛されるブランドを育む。"
+			/>
+			<meta
+				property="og:title"
+				content="クリエイティブ・エリア 砕区 -kudaku-"
+			/>
+			<meta
+				property="og:description"
+				content="砕区は、既存のやり方やルールに縛られないクリエイティブ・エリア。プロデューサー／映像作家／アートディレクター／写真家／空間デザイナーなどが交わりクライアントとの自由な発想とコラボレーションで心と人を動かすアウトプットを産み、愛されるブランドを育む。"
+			/>
+			<meta property="og:image" content="https://kudaku.tokyo/ogp_image.png" />
+			<meta property="og:image:width" content="1200" />
+			<meta property="og:image:height" content="630" />
+			<div>
+				<p>KUDAKU</p>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
