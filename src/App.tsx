d0ee5cd3d75acdbf681c71f8ace9d9acc0ea737b
tsunbow <kazuki_tsuyama@bita.jp>
2026-02-16 17:17:49 +0900

    メタタグをindex.htmlに移動

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,6 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<meta
-				name="description"
-				content="砕区は、既存のやり方やルールに縛られないクリエイティブ・エリア。プロデューサー／映像作家／アートディレクター／写真家／空間デザイナーなどが交わりクライアントとの自由な発想とコラボレーションで心と人を動かすアウトプットを産み、愛されるブランドを育む。"
-			/>
-			<meta
-				property="og:title"
-				content="クリエイティブ・エリア 砕区 -kudaku-"
-			/>
-			<meta
-				property="og:description"
-				content="砕区は、既存のやり方やルールに縛られないクリエイティブ・エリア。プロデューサー／映像作家／アートディレクター／写真家／空間デザイナーなどが交わりクライアントとの自由な発想とコラボレーションで心と人を動かすアウトプットを産み、愛されるブランドを育む。"
-			/>
-			<meta property="og:image" content="https://kudaku.tokyo/ogp_image.png" />
-			<meta property="og:image:width" content="1200" />
-			<meta property="og:image:height" content="630" />
 			<div>
 				<p>KUDAKU</p>
 			</div>
