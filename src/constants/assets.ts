7e298bfb0dcc591d2c8758e20e524cfc7f369b93
tsunbow <kazuki_tsuyama@bita.jp>
2026-07-05 00:58:56 +0900

    旧版を参照しベース設計を実装

---
+++ src/constants/assets.ts
@@ -0,0 +1,58 @@
+// Images
+import bgColor from "../assets/img/bg_color.png";
+import logoTop from "../assets/img/logo_top.svg";
+import logoRed from "../assets/img/logo_red.svg";
+import creativeArea from "../assets/img/creative-area.svg";
+import emailText from "../assets/img/email_text.svg";
+import copyButton from "../assets/img/copy_button.svg";
+import closeButton from "../assets/img/close_button.svg";
+import vector from "../assets/img/vector.png";
+import bg2 from "../assets/img/2-9.png";
+import letstalkBgSp from "../assets/img/letstalk_bg_sp.png";
+import letstalkSp from "../assets/img/letstalk_sp.svg";
+import playConceptPc from "../assets/img/play_concept_pc.svg";
+import playConceptSp from "../assets/img/play_concept_sp.svg";
+
+// Videos
+import trimVideo from "../assets/video/trim.mp4";
+import trim2Video from "../assets/video/trim2.mp4";
+import noiseVideo from "../assets/video/noise.mp4";
+
+// Lottie JSON
+import kuJson from "../assets/lottie/KU1.json";
+import ku2Json from "../assets/lottie/KU2.json";
+import daJson from "../assets/lottie/DA.json";
+import lineJson from "../assets/lottie/line.json";
+import talkHoverJson from "../assets/lottie/talk_hover.json";
+import talkHoverOutJson from "../assets/lottie/talk_hover_out.json";
+
+export const CommonAssets = {
+  bgColor,
+  logoTop,
+  logoRed,
+  creativeArea,
+  emailText,
+  copyButton,
+  closeButton,
+  vector,
+  trimVideo,
+  trim2Video,
+  noiseVideo,
+  kuJson,
+  ku2Json,
+  daJson,
+  lineJson,
+};
+
+export const PCAssets = {
+  bg: bg2,
+  playConceptPc,
+  talkHoverJson,
+  talkHoverOutJson,
+};
+
+export const SPAssets = {
+  letstalkBgSp,
+  letstalkSp,
+  playConceptSp,
+};
---
+++ src/pages/Top.tsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+
+// import Hero from "../sections/Hero";
+// import Statement from "../sections/Statement";
+// import WhereWeAre from "../sections/WhereWeAre";
+// import LetsTalk from "../sections/LetsTalk";
+
+const KudakuTop = () => (
+	<Fragment>
+		{/* Hero */}
+		<p>Hero（実装予定）</p>
+
+		{/* Statement */}
+		<p>Statement（実装予定）</p>
+
+		{/* WhereWeAre */}
+		<p>WhereWeAre（実装予定）</p>
+
+		{/* LetsTalk */}
+		<p>LetsTalk（実装予定）</p>
+	</Fragment>
+);
+
+export default KudakuTop;
